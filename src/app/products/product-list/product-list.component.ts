82c023bcd6d0c90e59ef0930edef24f2d9b319b5
ldex <laurent.duveau+git@gmail.com>
2022-06-02 19:46:31 -0400

    Fin du jour 1

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.interface';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  title: string = "Products";
+  products: Product[];
+  selectedProduct: Product;
+
+  onSelect(product: Product) {
+    this.selectedProduct = product;
+  }
+
+  constructor(
+    private productService: ProductService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this
+      .productService
+      .products$
+      .subscribe(
+        data => this.products = data
+      );
+  }
+
+}
